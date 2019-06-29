5f476c6cb7fc5cad194f6e08790cf156ab00342f
Kjuman Enobikto <gekyakibo@usako.net>
2019-06-30 02:23:03 +0900

    指摘された部分を直してみた
    キャメルケースをスネークケースにしたのと、thenは省けるらしいので省いた

--- main.rb
+++ main.rb
@@ -3,42 +3,42 @@ require "open-uri"
 
 # 日付受け取り
 puts "開始日をYYYYMMDDの形で入力してください"
-timeStart = gets.chomp
+time_start = gets.chomp
 puts "終了日をYYYYMMDDの形で入力してください"
-timeEnd = gets.chomp
+time_end = gets.chomp
 
 # timeオブジェクトへ変換
-startParsed = Date.parse(timeStart)
-endParsed = Date.parse(timeEnd)
+start_parsed = Date.parse(time_start)
+end_parsed = Date.parse(time_end)
 
-if endParsed <= startParsed then
+if end_parsed <= start_parsed
     puts "終了日が開始日より未来になっています"
     exit!
 end
 
 # timeオブジェクトを日本語表記に変換
-startStr = startParsed.strftime("%Y年%m月%d日")
-endStr = endParsed.strftime("%Y年%m月%d日")
+start_str = start_parsed.strftime("%Y年%m月%d日")
+end_str = end_parsed.strftime("%Y年%m月%d日")
 
 puts "ディレクトリを生成します"
 
 # ディレクトリがなければ作成
-if Dir.exist?("./so2stockdata") == false then
+if Dir.exist?("./so2stockdata") == false
     Dir.mkdir("./so2stockdata")
 end
-if Dir.exist?("./so2stockdata/buy_reports") == false then
+if Dir.exist?("./so2stockdata/buy_reports") == false
     Dir.mkdir("./so2stockdata/buy_reports")
 end
-if Dir.exist?("./so2stockdata/ranking") == false then
+if Dir.exist?("./so2stockdata/ranking") == false
     Dir.mkdir("./so2stockdata/ranking")
 end
-if Dir.exist?("./so2stockdata/ranking/all_top3_monthly") == false then
+if Dir.exist?("./so2stockdata/ranking/all_top3_monthly") == false
     Dir.mkdir("./so2stockdata/ranking/all_top3_monthly")
 end
-if Dir.exist?("./so2stockdata/ranking/top1000_monthly") == false then
+if Dir.exist?("./so2stockdata/ranking/top1000_monthly") == false
     Dir.mkdir("./so2stockdata/ranking/top1000_monthly")
 end
-if Dir.exist?("./so2stockdata/ranking/top1000_daily") == false then
+if Dir.exist?("./so2stockdata/ranking/top1000_daily") == false
     Dir.mkdir("./so2stockdata/ranking/top1000_daily")
 end
 departmentArray = ["point_total", "sale_total", "exp_stock", "exp_sale", "exp_job", "exp_trans", "exp_1", "exp_2", "exp_3", "exp_4", "exp_5", "exp_6", "exp_7", "exp_8", "exp_9", "exp_10", "exp_11", "exp_12", "exp_51", "exp_52", "exp_53", "exp_54", "exp_55", "exp_56", "exp_57", "exp_58", "exp_59", "exp_60", "exp_61", "exp_62", "exp_63", "exp_64", "exp_65", "exp_66", "exp_67", "exp_68", "exp_69", "exp_70"]
@@ -51,11 +51,11 @@ departmentArray = ["point_total", "sale_total", "exp_stock", "exp_sale", "exp_jo
     end
 end
 
-puts startStr + "から" + endStr + "までのデータを取得します"
+puts start_str + "から" + end_str + "までのデータを取得します"
 
 # 処理部(DLして保存)
 # レポート
-(Date.parse(timeStart)..Date.parse(timeEnd)).each do |date|
+(Date.parse(time_start)..Date.parse(time_end)).each do |date|
     ymd = date.strftime("%Y%m%d")
     if File.exist?("./so2stockdata/buy_reports/#{ymd}.json") == false
         begin
@@ -77,7 +77,7 @@ puts startStr + "から" + endStr + "までのデータを取得します"
 end
 
 # 月間全部門トップ3
-(Date.parse(timeStart)..Date.parse(timeEnd)).each do |date|
+(Date.parse(time_start)..Date.parse(time_end)).each do |date|
     ymd = date.strftime("%Y-%m")
     if date.strftime("%d") == "28"
         if File.exist?("./so2stockdata/ranking/all_top3_monthly/#{ymd}.json") == false
@@ -103,7 +103,7 @@ end
 end
 
 # 月間部門別トップ1000
-(Date.parse(timeStart)..Date.parse(timeEnd)).each do |date|
+(Date.parse(time_start)..Date.parse(time_end)).each do |date|
     ymd = date.strftime("%Y-%m")
     if date.strftime("%d") == "28"
         (departmentArray.length).times do |element|
@@ -130,7 +130,7 @@ end
 end
 
 # デイリートップ1000(部門別)
-(Date.parse(timeStart)..Date.parse(timeEnd)).each do |date|
+(Date.parse(time_start)..Date.parse(time_end)).each do |date|
     ymd = date.strftime("%Y-%m-%d")
     (departmentArray.length).times do |element|
         if File.exist?("./so2stockdata/ranking/top1000_daily/#{departmentArray[element]}/#{ymd}.json") == false
